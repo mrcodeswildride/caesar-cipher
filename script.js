01df6000d361830f5e09ee72d63c1786d3101ffa
David Keller <mrcodeswildride@gmail.com>
2020-08-26 16:28:35 +0000

    es6 update

--- script.js
+++ script.js
@@ -1,68 +1,67 @@
-var wordsInput = document.getElementById("words");
+let words = document.getElementById(`words`)
+let shiftAmount = document.getElementById(`shiftAmount`)
+let leftButton = document.getElementById(`leftButton`)
+let rightButton = document.getElementById(`rightButton`)
 
-var shiftAmountInput = document.getElementById("shiftAmount");
-var shiftLeftButton = document.getElementById("shiftLeft");
-var shiftRightButton = document.getElementById("shiftRight");
+leftButton.addEventListener(`click`, shiftLeft)
+rightButton.addEventListener(`click`, shiftRight)
 
-shiftLeftButton.addEventListener("click", shiftLeft);
-shiftRightButton.addEventListener("click", shiftRight);
-
-wordsInput.focus();
+words.focus()
 
 function shiftLeft() {
-    var amount = parseInt(shiftAmountInput.value, 10);
+  let amount = Number(shiftAmount.value)
 
-    if (!isNaN(amount)) {
-        shift(-amount);
-    }
+  if (!isNaN(amount)) {
+    shift(-amount)
+  }
 }
 
 function shiftRight() {
-    var amount = parseInt(shiftAmountInput.value, 10);
+  let amount = Number(shiftAmount.value)
 
-    if (!isNaN(amount)) {
-        shift(amount);
-    }
+  if (!isNaN(amount)) {
+    shift(amount)
+  }
 }
 
 function shift(amount) {
-    amount = amount % 26;
+  amount = amount % 26
 
-    var input = wordsInput.value.trim();
-    var output = "";
+  let input = words.value.trim()
+  let output = ``
 
-    for (var i = 0; i < input.length; i++) {
-        var charCode = input[i].charCodeAt();
+  for (let character of input) {
+    let charCode = character.charCodeAt()
 
-        if (charCode >= 65 && charCode <= 90) {
-            charCode += amount;
+    if (charCode >= 65 && charCode <= 90) {
+      charCode += amount
 
-            if (charCode < 65) {
-                charCode += 26;
-            }
-            else if (charCode > 90) {
-                charCode -= 26;
-            }
+      if (charCode < 65) {
+        charCode += 26
+      }
+      else if (charCode > 90) {
+        charCode -= 26
+      }
 
-            output += String.fromCharCode(charCode);
-        }
-        else if (charCode >= 97 && charCode <= 122) {
-            charCode += amount;
-
-            if (charCode < 97) {
-                charCode += 26;
-            }
-            else if (charCode > 122) {
-                charCode -= 26;
-            }
-
-            output += String.fromCharCode(charCode);
-        }
-        else {
-            output += input[i];
-        }
+      output += String.fromCharCode(charCode)
     }
+    else if (charCode >= 97 && charCode <= 122) {
+      charCode += amount
 
-    wordsInput.value = output;
-    wordsInput.focus();
+      if (charCode < 97) {
+        charCode += 26
+      }
+      else if (charCode > 122) {
+        charCode -= 26
+      }
+
+      output += String.fromCharCode(charCode)
+    }
+    else {
+      output += character
+    }
+  }
+
+  words.value = output
+  words.focus()
 }
