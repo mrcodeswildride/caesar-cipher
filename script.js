0322b0e39a08aa19627c331b99719498ba74515b
David Keller <kellerdb@gmail.com>
2021-05-19 08:59:04 -0700

    formatting

--- script.js
+++ script.js
@@ -10,58 +10,45 @@ words.focus()
 
 function shiftLeft() {
   let amount = Number(shiftAmount.value)
-
-  if (!isNaN(amount)) {
-    shift(-amount)
-  }
+  shift(-amount)
 }
 
 function shiftRight() {
   let amount = Number(shiftAmount.value)
-
-  if (!isNaN(amount)) {
-    shift(amount)
-  }
+  shift(amount)
 }
 
 function shift(amount) {
   amount = amount % 26
-
-  let input = words.value.trim()
   let output = ``
 
-  for (let character of input) {
-    let charCode = character.charCodeAt()
+  for (let char of words.value) {
+    let charCode = char.charCodeAt()
 
     if (charCode >= 65 && charCode <= 90) {
       charCode += amount
 
       if (charCode < 65) {
         charCode += 26
-      }
-      else if (charCode > 90) {
+      } else if (charCode > 90) {
         charCode -= 26
       }
 
       output += String.fromCharCode(charCode)
-    }
-    else if (charCode >= 97 && charCode <= 122) {
+    } else if (charCode >= 97 && charCode <= 122) {
       charCode += amount
 
       if (charCode < 97) {
         charCode += 26
-      }
-      else if (charCode > 122) {
+      } else if (charCode > 122) {
         charCode -= 26
       }
 
       output += String.fromCharCode(charCode)
-    }
-    else {
-      output += character
+    } else {
+      output += char
     }
   }
 
   words.value = output
-  words.focus()
 }
